4c5a2d6ed81a690cf05fe0fd5a3d56ccc69b8073
LeoDumas <leo.ndumas980@gmail.com>
2024-07-15 21:21:29 +0200

    Fix : return user id with the jwt + require userID to getAllTasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -6,41 +6,49 @@ import {
   Param,
   Delete,
   Put,
+  Req,
+  UseGuards,
 } from '@nestjs/common';
-import { tasksService } from './tasks.service';
+import { TasksService } from './tasks.service';
 import { Tasks } from './schemas/tasks.schema';
 import { CreateTaskDTO } from './dto/create-task.dto';
 import { UpdateTaskDTO } from './dto/update-task.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('tasks')
-export class tasksController {
-  constructor(private readonly tasksService: tasksService) {}
+@UseGuards(JwtAuthGuard)
+export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
 
   @Get()
-  async findAll(): Promise<Tasks[]> {
-    return this.tasksService.findAll();
+  async findAll(@Req() req): Promise<Tasks[]> {
+    return this.tasksService.findAll(req.user.userId);
   }
 
   @Get(':id')
-  async findOne(@Param('id') id: string): Promise<Tasks> {
-    return this.tasksService.findOne(id);
+  async findOne(@Param('id') id: string, @Req() req): Promise<Tasks> {
+    return this.tasksService.findOne(id, req.user.userId);
   }
 
   @Post()
-  async create(@Body() createTaskDTO: CreateTaskDTO): Promise<Tasks> {
-    return this.tasksService.create(createTaskDTO);
+  async create(
+    @Body() createTaskDTO: CreateTaskDTO,
+    @Req() req,
+  ): Promise<Tasks> {
+    return this.tasksService.create(createTaskDTO, req.user.userId);
   }
 
   @Delete(':id')
-  async deleteOne(@Param('id') id: string): Promise<void> {
-    return this.tasksService.deleteOne(id);
+  async deleteOne(@Param('id') id: string, @Req() req): Promise<void> {
+    return this.tasksService.deleteOne(id, req.user.userId);
   }
 
   @Put(':id')
   async updateOne(
     @Param('id') id: string,
     @Body() updateTaskDTO: UpdateTaskDTO,
+    @Req() req,
   ): Promise<Tasks> {
-    return this.tasksService.updateOne(id, updateTaskDTO);
+    return this.tasksService.updateOne(id, updateTaskDTO, req.user.userId);
   }
 }
